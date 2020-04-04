2423bd7a9165dae435b10770bd2679159ddc2f40
Alex <omareyes-96@hotmail.com>
2020-04-04 14:36:38 -0500

    creacion de modelos, migraciones y factories para poblar la base de datos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        //para llamar el seeder de usuarios y se cree el usuario
     }
 }
